64aea694338b5c5adeb1ea3d9b85107277732fae
Miguel <miguelarrepia@gmail.com>
2020-11-22 01:57:51 +0000

    tpc6 final - missing project record

--- tpc6/server.js
+++ tpc6/server.js
@@ -20,152 +20,59 @@ function recuperaInfo(request, callback){
 }
 
 // POST Confirmation HTML Page Template -------------------------------------
-function geraPostConfirm( aluno, d){
+function genPostConfirm( tarefa, d){
     return `
     <html>
-    <head>
-        <title>POST receipt: ${aluno.id}</title>
-        <meta charset="utf-8"/>
-        <link rel="icon" href="favicon.png"/>
-        <link rel="stylesheet" href="w3.css"/>
-    </head>
-    <body>
-        <div class="w3-card-4">
-            <header class="w3-container w3-teal">
-                <h1>Aluno ${aluno.id} inserido</h1>
-            </header>
-
-            <div class="w3-container">
-                <p><a href="/alunos/${aluno.id}">Aceda aqui à sua página."</a></p>
-            </div>
-
-            <footer class="w3-container w3-teal">
-                <address>Gerado por galuno::PRI2020 em ${d} - [<a href="/">Voltar</a>]</address>
-            </footer>
-        </div>
-    </body>
-    </html>
-    `
-}
-
-// Student List HTML Page Template  -----------------------------------------
-function geraPagAlunos( alunos, d){
-  let pagHTML = `
-    <html>
         <head>
-            <title>Lista de alunos</title>
+            <title>POST receipt: ${tarefa.id}</title>
             <meta charset="utf-8"/>
             <link rel="icon" href="favicon.png"/>
             <link rel="stylesheet" href="w3.css"/>
         </head>
         <body>
-            <div class="w3-container w3-teal">
-                <h2>Lista de Alunos
-                <button class="w3-button w3-circle w3-black w3-right">
-                <a href="/alunos/registo">+</a></button>
-                </h2>
+            <div class="w3-card-4">
+                <header class="w3-container w3-teal">
+                    <h1>Tarefa ${tarefa.id} inserida</h1>
+                </header>
+
+                <div class="w3-container">
+                    <p><a href="/">Voltar</a></p>
+                </div>
+
+                <footer class="w3-container w3-teal">
+                    <address>TODO List - PRI2020 em ${d}</address>
+                </footer>
             </div>
-            <table class="w3-table w3-bordered">
-                <tr>
-                    <th>Nome</th>
-                    <th>Número</th>
-                    <th>Curso</th>
-                </tr>
-  `
-  alunos.forEach( a => {
-    pagHTML += `
-        <tr>
-            <td><a href="/alunos/${a.id}">${a.nome}</a></td>
-            <td>${a.id}</td>
-            <td>${a.curso}</td>
-            <td><a href="${a.git}">${a.git}</a></td>
-        </tr>
-    `
-  })
-
-  pagHTML += `
-        </table>
-        <div class="w3-container w3-teal">
-            <address>Gerado por galuno::PRI2020 em ${d} --------------</address>
-        </div>
-    </body>
-    </html>
-  `
-  return pagHTML
-}
-
-// Student HTML Page Template -------------------------------------------------------
-function geraPagAluno( aluno, d ){
-    return `
-    <html>
-    <head>
-        <title>Aluno: ${aluno.id}</title>
-        <meta charset="utf-8"/>
-        <link rel="icon" href="favicon.png"/>
-        <link rel="stylesheet" href="w3.css"/>
-    </head>
-    <body>
-        <div class="w3-card-4">
-            <header class="w3-container w3-teal">
-                <h1>Aluno ${aluno.id}</h1>
-            </header>
-
-            <div class="w3-container">
-                <img src="/public/student.png" alt="Avatar" class="w3-left w3-circle w3-image" style="width:100%;max-width:100px">
-                <ul class="w3-ul w3-card-4" style="width:50%">
-                    <li><b>Nome: </b> ${aluno.nome}</li>
-                    <li><b>Número: </b> ${aluno.id}</li>
-                    <li><b>Curso: </b> ${aluno.curso}</li>
-                    <li><b>Git (link): </b> <a href="${aluno.git}">${aluno.git}</a></li>
-                </ul>
-            </div>
-
-            <footer class="w3-container w3-teal">
-                <address>Gerado por galuno::PRI2020 em ${d} - [<a href="/">Voltar</a>]</address>
-            </footer>
-        </div>
-    </body>
-    </html>
-    `
-}
-
-// Student Form HTML Page Template ------------------------------------------
-function geraFormAluno( d ){
-    return `
-    <html>
-        <head>
-            <title>Registo de um aluno</title>
-            <meta charset="utf-8"/>
-            <link rel="icon" href="favicon.png"/>
-            <link rel="stylesheet" href="w3.css"/>
-        </head>
-        <body>
-        
         </body>
-            <div class="w3-container w3-teal">
-                <h2>Registo de Aluno</h2>
+    </html>
+    `
+}
+
+
+// Canceled task Confirmation HTML Page Template -------------------------------------
+function genCanceledTask( tarefa, d){
+    return `
+    <html>
+        <head>
+            <title>Task ${tarefa.id} canceled</title>
+            <meta charset="utf-8"/>
+            <link rel="icon" href="favicon.png"/>
+            <link rel="stylesheet" href="w3.css"/>
+        </head>
+        <body>
+            <div class="w3-card-4">
+                <header class="w3-container w3-teal">
+                    <h1>Task ${tarefa.id} canceled</h1>
+                </header>
+
+                <div class="w3-container">
+                    <p><a href="/">Voltar</a></p>
+                </div>
+
+                <footer class="w3-container w3-teal">
+                    <address>TODO List - PRI2020 em ${d}</address>
+                </footer>
             </div>
-
-            <form class="w3-container" action="/alunos" method="POST">
-                <label class="w3-text-teal"><b>Nome</b></label>
-                <input class="w3-input w3-border w3-light-grey" type="text" name="nome">
-          
-                <label class="w3-text-teal"><b>Número / Identificador</b></label>
-                <input class="w3-input w3-border w3-light-grey" type="text" name="id">
-
-                <label class="w3-text-teal"><b>Curso</b></label>
-                <input class="w3-input w3-border w3-light-grey" type="text" name="curso">
-
-                <label class="w3-text-teal"><b>Link para o repositório no Git</b></label>
-                <input class="w3-input w3-border w3-light-grey" type="text" name="git">
-          
-                <input class="w3-btn w3-blue-grey" type="submit" value="Registar"/>
-                <input class="w3-btn w3-blue-grey" type="reset" value="Limpar valores"/> 
-            </form>
-
-            <footer class="w3-container w3-teal">
-                <address>Gerado por galuno::PRI2020 em ${d}</address>
-            </footer>
         </body>
     </html>
     `
@@ -188,12 +95,6 @@ function genRegTask(){
             <input class="w3-btn w3-blue-grey" type="reset" value="Limpar valores"/> 
         </form>
     `
-}
-
-// Cancel task
-// Sends DELETE request with the given ID
-function cancelTask(id){
-    
 }
 
 // Pending tasks list HTML Page Template  -----------------------------------------
@@ -355,7 +256,7 @@ var tarefasServer = http.createServer(function (req, res) {
                                 let a = response.data
                                 
                                 res.writeHead(200, {'Content-Type': 'text/html;charset=utf-8'})
-                                res.write(geraPagAluno(a, d))
+                                res.write(genCanceledTask(a, d))
                                 res.end()
                             })
                             .catch(function(erro){
@@ -389,7 +290,7 @@ var tarefasServer = http.createServer(function (req, res) {
                     axios.post('http://localhost:3000/tarefas', resultado)
                         .then(resp => {
                             res.writeHead(200, {'Content-Type': 'text/html;charset=utf-8'})
-                            res.write(geraPostConfirm( resp.data, d))
+                            res.write(genPostConfirm( resp.data, d))
                             res.end()
                         })
                         .catch(erro => {
